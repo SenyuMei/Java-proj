2db5ab6610456650de38c501ab73577d64bb93cc
dariomartnz <dario.martinez.m@estudiantat.upc.edu>
2025-05-18 11:33:49 +0200

    Add files via upload

---
+++ Parte 2/dominio/Competicion.java
@@ -0,0 +1,37 @@
+package edu.upc.etsetb.poo.decathlon1.dominio;
+
+/**
+ * Clase para gestionar la competición.
+ */
+public class Competicion {
+
+    private final String nombre;
+    private final String fecha;
+    private int numInscritos = 0;
+    private final String lugar;
+    
+    public Competicion(String nombre, String fecha, String lugar) {
+        this.nombre = nombre;
+        this.fecha = fecha;
+        this.lugar = lugar;
+    }
+
+    public int getNumInscritos() {
+        return this.numInscritos;
+    }
+
+    public int obtenerSiguienteNumInscripcion() {
+      return ++this.numInscritos;
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Nombre de la competicion: ").append(this.nombre).append("\n");
+        sb.append("Fecha de celebracion: ").append(this.fecha).append("\n");
+        sb.append("Lugar: ").append(this.lugar).append("\n");
+        sb.append("Numero de atletas inscritos: ").append(this.numInscritos).append("\n");
+        return sb.toString();
+    }
+
+}
---
+++ Parte 2/dominio/MarcaEnEvento.java
@@ -0,0 +1,216 @@
+package edu.upc.etsetb.poo.decathlon1.dominio;
+
+/**
+ * Clase para gestionar la marca obtenida en un evento por un atleta.
+ */
+public abstract class MarcaEnEvento {
+
+    /**
+     * Código para el evento 100 metros lisos.
+     */
+    public static final int CIEN_METROS = 0;
+    /**
+     * Código para el evento salto de longitud.
+     */
+    public static final int SALTO_DE_LONGITUD = 1;
+    /**
+     * Código para el evento lanzamiento de peso.
+     */
+    public static final int LAZAMIENTO_DE_PESO = 2;
+    /**
+     * Código para el evento salto de altura.
+     */
+    public static final int SALTO_DE_ALTURA = 3;
+    /**
+     * Código para el evento 400 metros lisos.
+     */
+    public static final int CUATROCIENTOS_METROS = 4;
+    /**
+     * Número total de eventos (5).
+     */
+    public static final int NUM_EVENTOS = 5;
+    /**
+     * Código para señalar que el método calcularPuntosEvento() ha podido 
+     * realizar los cálculos encomendados sin problemas.
+     */
+    public static final int CALCULO_EVENTO_OK = 0;
+    /**
+     * Código para señalar que se ha intentado calcular puntos para una 
+     * marca negativa, que es una situación que no debe darse.
+     */
+    public static final int CALCULO_EVENTO_KO_MARCA_NEGATIVA = -1;
+    
+    protected double marca;
+    
+    protected int puntos;
+    /**
+     * La normativa de Decatlon define 3 parámetros (A, B y C) que varían 
+     * según el evento en cuestión y forman parte de las fórmulas que permiten 
+     * calcular los puntos en función de una marca para cada uno de dichos 
+     * eventos; para más detalles, debéis consultar la página web
+     * 
+     * https://es.wikipedia.org/wiki/Decatl%C3%B3n#Sistema_de_puntos
+     * 
+    /**
+     * Matriz en la que se almacenan los valores de A, B y C para los 
+     * diferentes eventos según la normativa
+     * public static final double[][] PARAM = {<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;{25.4347, 18, 1.81}, //CIEN_METROS<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;{0.14354, 220, 1.40}, //SALTO_DE_LONGITUD<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;{51.39, 1.5, 1.05}, // LAZAMIENTO_DE_PESO<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;{0.8465, 75, 1.42}, // SALTO_DE_ALTURA<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;{1.53775, 82, 1.81}, // CUATROCIENTOS_METROS<br>
+     * };<br>
+     * Para acceder al parámetro A de CIEN_METROS es: PARAM[CIEN_METROS][A].
+     */
+    public static final double[][] PARAM = {
+        {25.4347, 18, 1.81}, //CIEN_METROS
+        {0.14354, 220, 1.40}, //SALTO_DE_LONGITUD
+        {51.39, 1.5, 1.05}, // LAZAMIENTO_DE_PESO
+        {0.8465, 75, 1.42}, // SALTO_DE_ALTURA
+        {1.53775, 82, 1.81}, // CUATROCIENTOS_METROS
+    };
+
+    /**
+     * Método constructor de la clase
+     *
+     * @param evento Evento
+     * @param marca Marca. La marca (o performance (o P)) es la marca obtenida
+     * por el atleta, medida en segundos (eventos de pista, es decir, carreras),
+     * metros (lanzamientos), o centímetros (saltos).
+     */
+    public MarcaEnEvento(double marca) {
+        this.marca = marca;
+        this.puntos = 0; // Inicialmente los puntos son 0, hasta que se calculen
+    }
+
+    /**
+     * Método getter. Devuelve los puntos de MarcaEnEvento.
+     *
+     * @return Devuelve los puntos de MarcaEnEvento.
+     */
+    public int getPuntos() {
+        return this.puntos;
+    }
+
+    /**
+     * Cálcula los puntos correspondientes a la marca en este evento y los
+     * asigna al atributo puntos. Los eventos de pista (carreras) y de los
+     * eventos de campo (lanzamientos y saltos) se calculan con una formula
+     * ligeramente diferente. Ver:
+     * https://es.wikipedia.org/wiki/Decatl%C3%B3n#Sistema_de_puntos para saber
+     * cómo se calculan.
+     *
+     * @return Devuelve CALCULO_EVENTO_OK o CALCULO_EVENTO_KO_MARCA_NEGATIVA.
+     */
+    public abstract void calcularPuntosEvento(double A, double B, double C, double marca) {
+        /*if (marca < 0) {
+            return CALCULO_EVENTO_KO_MARCA_NEGATIVA;
+        }*/
+        
+        double a = PARAM[evento][A];
+        double b = PARAM[evento][B];
+        double c = PARAM[evento][C];
+        
+        // Según las fórmulas de cálculo:
+        // - Eventos de pista (carreras): INT(A(B-P)^C)
+        // - Eventos de campo (saltos y lanzamientos): INT(A(P-B)^C)
+        
+        if (evento == CIEN_METROS || evento == CUATROCIENTOS_METROS) {
+            // Eventos de pista (carreras)
+            puntos = (int) Math.floor(a * Math.pow(b - marca, c));
+        } else {
+            // Eventos de campo (saltos y lanzamientos)
+            puntos = (int) Math.floor(a * Math.pow(marca - b, c));
+        }
+        
+        // Si da un resultado negativo, se pone a 0
+        if (puntos < 0) {
+            puntos = 0;
+        }
+        
+        return CALCULO_EVENTO_OK;
+    }
+
+    /**
+     * Devuelve un String con las únidades en las que se mide una marca de este
+     * tipo de evento: segundos (para eventos de pista), metros (para
+     * lanzamientos), y centímetros (para saltos).
+     *
+     * @param evento El tipo de evento.
+     * @return El String con el tipo de únidades ("segundos", "metros" o
+     * "centímetros".
+     */
+    public static String getUnidadMarcaEnEvento(int evento) {
+        switch (evento) {
+            case CIEN_METROS:
+            case CUATROCIENTOS_METROS:
+                return "segundos";
+            case LAZAMIENTO_DE_PESO:
+                return "metros";
+            case SALTO_DE_LONGITUD:
+            case SALTO_DE_ALTURA:
+                return "centimetros";
+            default:
+                throw new IllegalArgumentException("Evento no válido: " + evento);
+        }
+    }
+
+    /**
+     * Devuelve un String con la lista de eventos y sus números de evento
+     * correspondiente.
+     *
+     * @return Dicho String.
+     */
+    public static String getListaEventos() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Lista de eventos:\n");
+        sb.append("[0]    100 metros lisos\t");
+        sb.append("[1]    Salto de longitud\t");
+        sb.append("[2]    Lanzamiento de peso\t");
+        sb.append("[3]    Salto de altura\t");
+        sb.append("[4]    400 metros lisos");
+        return sb.toString();
+    }
+
+    /**
+     * Método toString() de la clase.
+     *
+     * @return String con la información de la marca; a continuación se 
+     * muestra un ejemplo:<br>
+     * 100 metros lisos:     marca=11.278 segundos, puntos=800
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        String nombreEvento;
+
+        switch (evento) {
+            case CIEN_METROS:
+                nombreEvento = "100 metros lisos";
+                break;
+            case SALTO_DE_LONGITUD:
+                nombreEvento = "Salto de longitud";
+                break;
+            case LAZAMIENTO_DE_PESO:
+                nombreEvento = "Lanzamiento de peso";
+                break;
+            case SALTO_DE_ALTURA:
+                nombreEvento = "Salto de altura";
+                break;
+            case CUATROCIENTOS_METROS:
+                nombreEvento = "400 metros lisos";
+                break;
+            default:
+                nombreEvento = "Evento desconocido";
+        }
+
+        sb.append(nombreEvento);
+        sb.append(":      marca=");
+        sb.append(marca);  // No se cambia nada, imprimirá con punto si el sistema lo hace así
+        sb.append(" ").append(getUnidadMarcaEnEvento(evento));
+        sb.append(", puntos=").append(puntos);
+
+        return sb.toString();
+    }
+ }
