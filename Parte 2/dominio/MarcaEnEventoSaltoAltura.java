762c12e19dc2d8ac4f708f26029416adf8c89ce5
dariomartnz <dario.martinez.m@estudiantat.upc.edu>
2025-05-19 16:00:42 +0200

    Update MarcaEnEventoSaltoAltura.java

--- Parte 2/dominio/MarcaEnEventoSaltoAltura.java
+++ Parte 2/dominio/MarcaEnEventoSaltoAltura.java
@@ -8,20 +8,28 @@ package edu.upc.etsetb.poo.decathlon1.dominio;
  *
  * @author dario
  */
-public class MarcaEnEventoSaltoLongitud extends MarcaEnEventoDeCampo {
+public class MarcaEnEventoSaltoAltura extends MarcaEnEventoDeCampo {
     public static double A;
     public static double B;
     public static double C;
     
-    public MarcaEnEventoSaltoLongitud(double marca) {
+    
+    public MarcaEnEventoSaltoAltura(double marca) {
         super(marca);
-    }
-
+        calcularPuntosEvento(PARAM[SALTO_DE_ALTURA][0], 
+                         PARAM[SALTO_DE_ALTURA][1], 
+                         PARAM[SALTO_DE_ALTURA][2], 
+                         this.marca);
+    }   
+    
     @Override
     public String toString(){
         StringBuilder sb = new StringBuilder();
-        sb.append("Salto de Longitud:").append("marca = ").append("a").append("centimetros").append(getPuntos());
+        sb.append("Salto de Altura: ");
+        sb.append("marca = ").append(this.marca).append(" segundos,");
+        sb.append("puntos = ").append(this.puntos);
         return sb.toString();
     }
     
 }
+
