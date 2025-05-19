bf690fefdfece3667fbeb5ec2ea76410ad12dbed
dariomartnz <dario.martinez.m@estudiantat.upc.edu>
2025-05-19 13:51:13 +0200

    Update Atleta.java

--- Parte 2/dominio/Atleta.java
+++ Parte 2/dominio/Atleta.java
@@ -1,50 +1,114 @@
- package edu.upc.etsetb.poo.decathlon1.dominio;
-
-import java.util.LinkedList;
+package edu.upc.etsetb.poo.decathlon1.dominio;
 
 /**
- * Clase para gestionar la clasificación; solo mostrará, ordenados, un número 
- * determinado de atletas (que puede ser diferente al número de participantes) pasado 
- * como argumento al constructor.
+ * Clase para gestionar la información de un atleta
+ * 
  */
-public class Clasificacion {
+public class Atleta {
 
-    private final int numAtletas;
-    private LinkedList<Atleta> atletas = new LinkedList(); // revisar final || no
+    private final int numInscripcion;
+    private final String nombre;
+    private final String nacionalidad;
+    private int puntos;
+    private final MarcaEnEvento[] marcas;
+
     /**
-     * Método constructor de la clase. Se le pasa el número de atletas que
-     * tendrá la clasificación y crea la lista , de momento vacia, de atletas.
-     *
-     * @param numAtletas El número de atletas que se incluirán en el String 
-     * que devolverá el método toString().
+     * Método constructor de la clase
+
      */
-    public Clasificacion(int numAtletas) {
-        this.numAtletas = numAtletas;
-        this.atletas = new LinkedList<>();
+    public Atleta(String nombre, String nacionalidad, int numInscripcion) {
+        this.nombre = nombre;
+        this.nacionalidad = nacionalidad;
+        this.numInscripcion = numInscripcion;
+        this.marcas = new MarcaEnEvento[MarcaEnEvento.NUM_EVENTOS];
     }
+
     /**
-     * Añade ordenadamente el atleta a a la clasificación (de acuerdo a los
-     * puntos totales del atleta) y, si el número de atletas en la
-     * clasificación ya supera el guardado en el atributo numAtletas, elimina
-     * el atleta con menos puntos. De este modo, si se crea una clasificación y
-     * se van añadiendo todos los atletas utilizando este método, al final
-     * quedarán los numAtletas con mejor puntuación ordenados por dicha
-     * puntuación.
+     * Método getter. Devuelve el número de inscripción.
      *
-     * @param a El atleta añadir a la clasificación
+     *
      */
-    public void anyadirAClasificacion(Atleta a) {
-        int recorrido = 0;
-        
-        if ( atletas.isEmpty() ) {
-            atletas.add(a);
-        } else {
-            while ( recorrido < atletas.size() && atletas.get(recorrido).getPuntos() > a.getPuntos()) {
-                recorrido++;
+    public int getNumInscripcion() {
+      return this.numInscripcion;
+    }
+
+    /**
+     * Método getter. Devuelve el nombre del atleta.
+     *
+     *
+     */
+    public String getNombre() {
+      return this.nombre;
+    }
+
+    /**
+     * Método getter. Devuelve la nacionalidad del atleta.
+     *
+     */
+    public String getNacionalidad() {
+       return this.nacionalidad;
+       
+       }
+
+    /**
+     * Método getter. Devuelve la suma de los puntos obtenidos por el atleta en
+     * las diferentes pruebas en las que ha participado.
+     *
+     */
+    public int getPuntos() {
+       return this.puntos;
+    }
+
+    /**
+     * Crea una nueva MarcaEnEvento, la añade en la posición evento del vector
+     * marcas, calcula los puntos correspondientes a ese evento para este atleta
+     * y recalcula los puntos totales del atleta.
+     *
+     * @param evento el evento en el que el atleta ha conseguido la marca
+     * @param marca la marca conseguida por el atleta
+     */
+    public void anyadirMarcaEnEvento(int evento, double marca) throws MarcaNegativaException {
+       if (marca < 0) {
+           throw new MarcaNegativaException("La marca no puede ser negativa.");
+       }
+        MarcaEnEvento nuevaMarca = null;
+        switch (evento) {
+            case MarcaEnEvento.CIEN_METROS:
+                nuevaMarca = new MarcaEnEvento100m(marca);
+                break;
+            case MarcaEnEvento.SALTO_DE_LONGITUD:
+                nuevaMarca = new MarcaEnEventoSaltoLongitud(marca);
+                break;
+            case MarcaEnEvento.LAZAMIENTO_DE_PESO:
+                nuevaMarca = new MarcaEnEventoLanzamientoPeso(marca);
+                break;
+            case MarcaEnEvento.SALTO_DE_ALTURA:
+                nuevaMarca = new MarcaEnEventoSaltoAltura(marca);
+                break;
+            case MarcaEnEvento.CUATROCIENTOS_METROS:
+                nuevaMarca = new MarcaEnEvento400m(marca);
+                break;
+            default:
+                System.out.println("Evento Incorrecto!");
+                break;
             }
-            atletas.add(recorrido, a);
-            if(atletas.size() > numAtletas) {
-                atletas.removeLast();
+    
+    double[] parametros = MarcaEnEvento.PARAM[evento];
+    nuevaMarca.calcularPuntosEvento(parametros[0], parametros[1], parametros[2], marca);
+    marcas[evento] = nuevaMarca;
+    calcularPuntos();          
+     
+    }
+
+
+    /**
+     * Recalcula los puntos totales obtendos por el atleta hasta el momento.
+     */
+    public void calcularPuntos() {
+        this.puntos = 0;
+        for (MarcaEnEvento marca : this.marcas) {
+            if (marca != null) {
+                this.puntos += marca.getPuntos();
             }
         }
     }
@@ -52,23 +116,36 @@ public class Clasificacion {
     /**
      * Método toString() de la clase.
      *
-     * @return Devuelve la clasificación con, para cada atleta, número de
-     * clasificación, nacionalidad y nombre; a continuación se muestra un 
-     * ejemplo si el valor del atributo numAtletas es 5:<br>
-     * 1 ALG Larbi Bourrada<br>
-     * 2 KAZ Dmitriy Karpov<br>
-     * 3 FRA Kevin Mayer<br>
-     * 4 USA Ashton Eaton<br>
-     * 5 AUS Ashley Moloney<br>
-     */
-    @Override
-    public String toString() {
+     * String con la información del atleta. A continuación se muestra
+     * un ejemplo de su contenido:<br><br>
+     * Número de inscripción: 1<br>
+     * Nombre: Pepe Pérez<br>
+     * Nacionalidad:ES<br>
+     * 100 metros lisos:     marca=11.278 segundos, puntos=800<br>
+     * Salto de longitud:    marca=694.0 centimetros, puntos=799<br>
+     * Lanzamiento de peso:  marca=15.16 metros, puntos=800<br>
+     * Salto de altura:      marca=199.0 centimetros, puntos=794<br>
+     * 400 metros lisos:     marca=50.32 segundos, puntos=800<br>
+     * <br>
+     * Puntos totales: 3993<br>
+     * 
+        */
+   @Override
+   public String toString() {
        StringBuilder sb = new StringBuilder();
-       for (int i = 0; i < atletas.size(); i++) {
-            Atleta atleta = atletas.get(i);
-            sb.append(i + 1).append(" ").append(atleta.getNacionalidad()).append(" ").append(atleta.getNombre()).append("\n");
-        }
-        return sb.toString();
-    }
+       sb.append("Numero de inscripcion: ").append(this.numInscripcion).append("\n");
+       sb.append("Nombre: ").append(this.nombre).append("\n");
+       sb.append("Nacionalidad: ").append(this.nacionalidad).append("\n");
+
+       for (MarcaEnEvento marca : marcas) {
+           if (marca != null) {
+               sb.append(marca.toString()).append("\n");
+           }
+       }
+
+       sb.append("\nPuntos totales: ").append(this.puntos).append("\n");
+
+       return sb.toString();
+   }
 
 }
