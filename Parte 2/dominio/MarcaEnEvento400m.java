78fa832d956e6390717dc15cf95245549cd2f380
dariomartnz <dario.martinez.m@estudiantat.upc.edu>
2025-05-19 15:55:42 +0200

    Update MarcaEnEvento400m.java

--- Parte 2/dominio/MarcaEnEvento400m.java
+++ Parte 2/dominio/MarcaEnEvento400m.java
@@ -16,11 +16,18 @@ public class MarcaEnEvento400m extends MarcaEnEventoDePista {
 
     public MarcaEnEvento400m(double marca) {
         super(marca);
+        calcularPuntosEvento(PARAM[CUATROCIENTOS_METROS][0], 
+                         PARAM[CUATROCIENTOS_METROS][1], 
+                         PARAM[CUATROCIENTOS_METROS][2], 
+                         this.marca);
     }   
     
     @Override
     public String toString(){
         StringBuilder sb = new StringBuilder();
+        sb.append("400 metros lisos: ");
+        sb.append("marca = ").append(this.marca).append(" segundos,");
+        sb.append("puntos = ").append(this.puntos);
         return sb.toString();
     }
 }
